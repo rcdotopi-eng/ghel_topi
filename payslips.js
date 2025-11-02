4fd489b5ecde5b23e7b380f915f3a8217af99a88
rcdotopi-eng <rcdotopi@gmail.com>
2025-11-02 15:04:35 +0500

    Update payslips.js

--- payslips.js
+++ payslips.js
@@ -21,12 +21,41 @@ const payslipFiles = {
     "payslips/2025/08/90155365.pdf"
   ],
   "09": [
-    "payslips/2025/09/90048454.pdf",
-    "payslips/2025/09/90060820.pdf"
-    // … and so on
+    "payslips/2025/08/90048454.pdf",
+    "payslips/2025/08/90060820.pdf",
+    "payslips/2025/08/90061045.pdf",
+    "payslips/2025/08/90062043.pdf",
+    "payslips/2025/08/90062328.pdf",
+    "payslips/2025/08/90082748.pdf",
+    "payslips/2025/08/90113187.pdf",
+    "payslips/2025/08/90114686.pdf",
+    "payslips/2025/08/90115627.pdf",
+    "payslips/2025/08/90117213.pdf",
+    "payslips/2025/08/90121276.pdf",
+    "payslips/2025/08/90121405.pdf",
+    "payslips/2025/08/90123590.pdf",
+    "payslips/2025/08/90129707.pdf",
+    "payslips/2025/08/90130796.pdf",
+    "payslips/2025/08/90134941.pdf",
+    "payslips/2025/08/90155365.pdf"
   ],
   "10": [
-    "payslips/2025/10/90048454.pdf"
-    // … add all other staff PDFs for that month
+    "payslips/2025/08/90048454.pdf",
+    "payslips/2025/08/90060820.pdf",
+    "payslips/2025/08/90061045.pdf",
+    "payslips/2025/08/90062043.pdf",
+    "payslips/2025/08/90062328.pdf",
+    "payslips/2025/08/90082748.pdf",
+    "payslips/2025/08/90113187.pdf",
+    "payslips/2025/08/90114686.pdf",
+    "payslips/2025/08/90115627.pdf",
+    "payslips/2025/08/90117213.pdf",
+    "payslips/2025/08/90121276.pdf",
+    "payslips/2025/08/90121405.pdf",
+    "payslips/2025/08/90123590.pdf",
+    "payslips/2025/08/90129707.pdf",
+    "payslips/2025/08/90130796.pdf",
+    "payslips/2025/08/90134941.pdf",
+    "payslips/2025/08/90155365.pdf"
   ]
 };
